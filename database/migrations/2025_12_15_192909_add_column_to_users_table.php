645a1b7c5b55656cf1a3675d681f795b7b091af1
mkayf <muhammadkaif150@gmail.com>
2026-04-16 23:50:01 +0500

    workin

--- database/migrations/2025_12_15_192909_add_column_to_users_table.php
+++ database/migrations/2025_12_15_192909_add_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->addColumn('string', 'provider');
+            $table->string('provider')->default('credential');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('provider');
         });
     }
 };
