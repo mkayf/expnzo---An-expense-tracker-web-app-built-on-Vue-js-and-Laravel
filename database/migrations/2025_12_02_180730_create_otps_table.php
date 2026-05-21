f9d66087aa38e3614c8bfdab29cf1e301840e41f
mkayf <muhammadkaif150@gmail.com>
2026-05-21 23:50:33 +0500

    working

--- database/migrations/2025_12_02_180730_create_otps_table.php
+++ database/migrations/2025_12_02_180730_create_otps_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('otps', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
             $table->string('code');
             $table->integer('attempts')->default(0);
             $table->dateTime('resend_allowed_at');
