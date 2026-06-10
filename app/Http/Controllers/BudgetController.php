7726205d60a0ce870a11141762339bcef2baf8da
muhammad kaif <muhammadkaif150@gmail.com>
2026-06-10 00:28:50 -0700

    budget module handling

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\BudgetService;
 use Illuminate\Http\Request;
 
 class BudgetController extends Controller
 {
-    
+    public function __construct(protected BudgetService $budgetService)
+    {
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'limit_amount' => ['required', 'numeric', 'min:1', 'max:999,999,999,999']
+        ]);        
+    }
+
 }
--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Budget extends Model
 {
-    protected $fillable = ['user_id', 'month', 'limit_amount'];
+    protected $fillable = ['user_id', 'period', 'limit_amount'];
 
     public function user(){
         return $this->belongsTo(User::class, 'user_id');
