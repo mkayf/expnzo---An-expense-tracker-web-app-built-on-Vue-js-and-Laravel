3c516f4ae75193c352e9b81d35bd042fe3d6695c
muhammad kaif <muhammadkaif150@gmail.com>
2026-06-19 01:36:01 -0700

    workin

--- app/Services/FinancialSummaryService.php
+++ app/Services/FinancialSummaryService.php
@@ -2,18 +2,35 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+
 class FinancialSummaryService
 {
     public function statsSummary($user){
         $total_incomes = $user->transactions()->where('type', 'income')->sum('amount');
         $total_expenses = $user->transactions()->where('type', 'expense')->sum('amount');
-        $total_balance = $total_incomes - $total_expenses;
+        $total_balance =  $total_incomes - $total_expenses;
+
+        $current_date = Carbon::now();
+
+        $last_month_income = $user->transactions()->where('type', 'income')->whereBetween('transaction_date', [$current_date->copy()->subMonthNoOverflow()->startOfMonth(), $current_date->copy()->subMonthNoOverflow()->endOfMonth()])->sum('amount');
+
+        $last_month_expense = $user->transactions()->where('type', 'expense')->whereBetween('transaction_date', [$current_date->copy()->subMonthNoOverflow()->startOfMonth(), $current_date->copy()->subMonthNoOverflow()->endOfMonth()])->sum('amount');
+
+        $last_month_balance = $last_month_income - $last_month_expense;
+
+        $current_month_income = $user->transactions()->where('type', 'income')->whereBetween('transaction_date', [$current_date->copy()->startOfMonth(), $current_date->copy()])->sum('amount');
+
+        $current_month_expense = $user->transactions()->where('type', 'expense')->whereBetween('transaction_date', [$current_date->copy()->startOfMonth(), $current_date->copy()])->sum('amount');
+
+        $current_month_balance = $current_month_income - $current_month_expense;
 
         return [
-            [
+            'balance' => [
                 'label' => 'Total Balance',
                 'total_balance' => $total_balance,
-                
+                'last_month_balance' => $last_month_balance,
+                'current_month_balance' => $current_month_balance,
             ]
         ];
 
