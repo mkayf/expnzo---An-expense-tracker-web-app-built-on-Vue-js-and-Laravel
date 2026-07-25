c23d86cb834a3532197a450ba6b7bd3c9f64f277
muhammad kaif <muhammadkaif150@gmail.com>
2026-07-25 07:44:15 -0700

    before element-plus upgrade

--- resources/js/src/components/BudgetForm.vue
+++ resources/js/src/components/BudgetForm.vue
@@ -1,11 +1,17 @@
 <template>
-    <el-dialog v-model="dialogVisible"   width="400" align-center>
+    <el-dialog v-model="dialogVisible" width="400" align-center>
         <template #header>
             <h2 class="text-lg font-semibold">Set monthly budget</h2>
         </template>
         <span class="">Set the maximum amount you want to spend this month.</span>
-        <div>
-            <el-input-number v-model="num" :min="1" :max="10" size="large" />
+        <div class="py-4 flex items-center">
+            <div class="flex-1">
+                <span class="font-semibold text-xl text-black">{{ userCurrency }}</span>
+            </div>
+            <div class="flex-4">
+                <el-input-number style="width: 100%;" v-model="num" :precision="2" :step="0.01" :formatter="formatted"
+                    :parser="parsed" />
+            </div>
         </div>
         <template #footer>
             <div class="dialog-footer">
@@ -20,7 +26,7 @@
 
 <script setup>
 import { computed, ref } from 'vue'
-
+import useAuthStore from '../stores/auth';
 const props = defineProps({
     visible: {
         type: Boolean,
@@ -30,8 +36,10 @@ const props = defineProps({
 
 const emit = defineEmits(['close-dialog']);
 
-const num = ref(0);
+const authStore = useAuthStore();
 
+const num = ref(2);
+const userCurrency = authStore.user?.preferences?.currency ?? 'PKR';
 
 const dialogVisible = computed({
     get: () => props.visible,
@@ -43,5 +51,17 @@ const dialogVisible = computed({
 const closeDialog = () => {
     emit('close-dialog');
 }
+
+const formatted = (value) => {
+    console.log('value: ', value);
+    if (value === null || value === undefined || value === '') return ''
+    return String(value).replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+const parsed = (value) => {
+  if (!value) return ''
+  return value.replace(/,/g, '')
+}
+
 
 </script>
